82d2b9c9f1f00cae035f705c787b600256fb8888
Replit user <replituser@example.com>
2021-03-13 08:03:58 +0000

    Added multiple call of an user created function

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ def greet_user():
   name=input()
   print("Hello", name)
 #Line 2 is the actual function.This line will not be executed without calling the function.
-#To call the function we simply use its name like this
+#To call the function we simply use its name once.You can call a function multiple time.If you want to execute the function 3 times, the programm will look  like this:
+greet_user()
+greet_user()
 greet_user()
